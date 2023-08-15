7717b48d43f2478160f08617c35ef36676608b3b
David Merfield <dmerfield@gmail.com>
2023-08-15 11:37:37 +0100

    Use new API

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 var app = require("electron").app;
 const globalShortcut = require("electron").globalShortcut;
-var { PanelWindow } = require("electron-panel-window");
+const { BrowserWindow } = require('electron')
 var mainWindow = null;
 const path = require("path");
 
 app.dock.hide();
 
 app.on("ready", function() {
-  mainWindow = new PanelWindow({
+  mainWindow = new BrowserWindow({
     center: true,
     width: 600,
     height: 100,
     minHeight: 100,
+    frame: false,
     minWidth: 100,
     show: false,
-    webPreferences: {
-      preload: path.join(app.getAppPath(), "preload.js")
-    }
+    type: 'panel',
+    // webPreferences: {
+    //   preload: path.join(app.getAppPath(), "preload.js")
+    // }
   });
 
   mainWindow.loadURL("file://" + __dirname + "/index.html");
